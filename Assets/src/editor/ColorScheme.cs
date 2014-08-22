b904d470fac61eb087403da2684cba373ad94047
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-08-21 23:47:21 -0300

    Fix Indentation spaces

--- Assets/src/editor/ColorScheme.cs
+++ Assets/src/editor/ColorScheme.cs
@@ -4,45 +4,45 @@
 /// Syntax HighLight Code
 /// </summary>
 public static class ColorScheme  
-{		
-	/// <summary>
-	/// The color of the background.
-	/// </summary>
-	public static Color32 Background  = new Color32(42, 50, 50, 255);
-	
-	/// <summary>
-	/// The background color2.
-	/// </summary>
-	public static Color32 Background2 = new Color32(34, 34, 34, 255);
-	
-	/// <summary>
-	/// The background color3.
-	/// </summary>
-	public static Color32 Background3 = new Color32(39, 40, 34, 255);
-	
-	/// <summary>
-	/// White Letters
-	/// </summary>
-	public static Color32 White  = new Color32(255, 255, 255, 255);
-								 
-	/// <summary>			    
-	/// Pink Keywords		    
-	/// </summary>			   
-	public static Color32 Pink   = new Color32(244, 0,   101, 255);
-								 
-	/// <summary>			    
-	/// Molokai Green		    
-	/// </summary>			   
-	public static Color32 Green  = new Color32(165, 255, 11 , 255);
-	
-	/// <summary>
-	/// Orange Strings
-	/// </summary>
-	public static Color32 Orange = new Color32(237, 158, 38, 255);
-	
-	/// <summary>
-	/// Comments
-	/// </summary>
-	public static Color32 Gray   = new Color32(100, 100, 100, 255);
-	
+{       
+    /// <summary>
+    /// The color of the background.
+    /// </summary>
+    public static Color32 Background  = new Color32(42, 50, 50, 255);
+
+    /// <summary>
+    /// The background color2.
+    /// </summary>
+    public static Color32 Background2 = new Color32(34, 34, 34, 255);
+
+    /// <summary>
+    /// The background color3.
+    /// </summary>
+    public static Color32 Background3 = new Color32(39, 40, 34, 255);
+
+    /// <summary>
+    /// White Letters
+    /// </summary>
+    public static Color32 White  = new Color32(255, 255, 255, 255);
+
+    /// <summary>               
+    /// Pink Keywords           
+    /// </summary>             
+    public static Color32 Pink   = new Color32(244, 0,   101, 255);
+
+    /// <summary>               
+    /// Molokai Green           
+    /// </summary>             
+    public static Color32 Green  = new Color32(165, 255, 11 , 255);
+
+    /// <summary>
+    /// Orange Strings
+    /// </summary>
+    public static Color32 Orange = new Color32(237, 158, 38, 255);
+
+    /// <summary>
+    /// Comments
+    /// </summary>
+    public static Color32 Gray   = new Color32(100, 100, 100, 255);
+
 }

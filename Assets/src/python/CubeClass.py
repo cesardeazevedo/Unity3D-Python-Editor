7dd6447f373785a5386d0c0517258c539118d60f
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-11-29 23:16:18 -0200

    added python example

--- Assets/src/python/CubeClass.py
+++ Assets/src/python/CubeClass.py
@@ -1,12 +1,18 @@
 ﻿import UnityEngine as unity
 
+class CubeClass():
+    
+    vel = 1.5
+    
+    def Awake(self, this):
+        unity.Debug.Log("Awake Method")
 
-class Cube():
-    def Start():
-        pass
+    def Start(self, this):
+        unity.Debug.Log("Start Method")
+        this.renderer.material.color = unity.Color.red      
+    
+    def Update(self, this):
+        horizontal = unity.Input.GetAxis("Horizontal") * self.vel * unity.Time.deltaTime
+        vertical   = unity.Input.GetAxis("Vertical")   * self.vel * unity.Time.deltaTime
+        this.transform.Translate(horizontal,vertical, 0)
 
-    def Update():
-        pass
-        
-    def Test():
-        pass

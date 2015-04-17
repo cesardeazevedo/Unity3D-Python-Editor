146968d65a341228e365736e688c0a7c080c97e5
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-04-17 02:58:29 -0300

    added GetComponent rigidbody example

---
+++ Assets/src/python/RigidbodyExample.py
@@ -0,0 +1,14 @@
+﻿import UnityEngine as u
+
+class RigidbodyExample():
+
+    rigid = None
+
+    def Awake(self, this):
+        self.rigid = this.GetComponent(u.Rigidbody)      
+
+    def Update(self, this):
+ 
+        if(u.Input.GetKeyDown(u.KeyCode.Space)):
+        	self.rigid.velocity = u.Vector3(0, 5, 0)
+
